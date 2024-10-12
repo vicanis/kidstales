bde212a1a2c5bc37c1b45e5c24f0dccbef29b06f
Viktor Anisimov <vicanis@gmail.com>
2024-10-13 00:39:11 +0400

    Добавлен режим буклета для просмотра двух страниц книги в ландшафтной ориентации

--- static/script.js
+++ static/script.js
@@ -1,19 +1,25 @@
 function init() {
-    const img = $('.book-card .book-image img');
+    const image = $('.book-card .book-image');
 
-    const { base, count } = img.data();
+    const { base, count } = image.data();
 
-    img.attr({ src: `${base}/1.jpg` });
+    setImages(0, undefined);
 
     const leftArrow = $('.book-card .arrow.left');
     const rightArrow = $('.book-card .arrow.right');
+
+    $('.book-card .booklet-mode').click(function() {
+        image.toggleClass('booklet');
+        $(this).toggleClass('enabled');
+        invalidate();
+    });
 
     $('.book-card .arrow').click(function() {
         if ($(this).hasClass('disabled')) {
             return;
         }
 
-        const { page } = img.data();
+        const { page } = image.data();
 
         let newPage = page;
 
@@ -23,12 +29,8 @@ function init() {
             }
             
             newPage--;
-
-            rightArrow.removeClass('disabled');
-            if (newPage == 0) {
-                leftArrow.addClass('disabled');
-            } else {
-                leftArrow.removeClass('disabled');
+            if (image.hasClass('booklet')) {
+                newPage--;
             }
         } else if ($(this).is('.right')) {
             if (page == count - 1) {
@@ -36,16 +38,58 @@ function init() {
             }
 
             newPage++;
-
-            leftArrow.removeClass('disabled');
-            if (newPage == count - 1) {
-                rightArrow.addClass('disabled');
-            } else {
-                rightArrow.removeClass('disabled');
+            if (image.hasClass('booklet')) {
+                newPage++;
             }
         }
 
-        img.data({ page: newPage });
-        img.attr({ src: `${base}/${newPage+1}.jpg` });
+        image.data({ page: newPage });
+
+        invalidate();
     });
+
+    function invalidate() {
+        const { page } = image.data();
+
+        switch (page) {
+            case 0:
+                leftArrow.addClass('disabled');
+                rightArrow.removeClass('disabled');
+                break;
+            
+            case count-1:
+                leftArrow.removeClass('disabled');
+                rightArrow.addClass('disabled');
+                break;
+
+            default:
+                leftArrow.removeClass('disabled');
+                rightArrow.removeClass('disabled');
+                break;
+        }
+
+        const [ pageLeft, pageRight ] = calcNewPages(page);
+
+        setImages(pageLeft, pageRight);
+    }
+
+    function calcNewPages(page) {
+        const isBookletMode = image.hasClass('booklet');
+
+        if (page == 0 || !isBookletMode || page == count-1) {
+            return [page];
+        }
+
+        return [page, page+1];
+    }
+
+    function setImages(pageLeft, pageRight) {
+        image.find('img.left').attr({ src: `${base}/${pageLeft+1}.jpg` });
+
+        if (pageRight) {
+            image.find('img.right').show().attr({ src: `${base}/${pageRight+1}.jpg` });
+        } else {
+            image.find('img.right').hide();
+        }
+    }
 }
