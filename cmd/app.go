3034c43a9598faa180db9699569de7438acea1db
Viktor Anisimov <vicanis@gmail.com>
2025-09-20 22:23:42 +0400

    SIGINT context cancel

--- cmd/app.go
+++ cmd/app.go
@@ -4,11 +4,23 @@ import (
 	"context"
 	"kidstales/internal/app"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
+
+	// Настройка канала для получения сигналов
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		<-signalChan // Ожидание сигнала
+		cancel()     // Отмена контекста
+	}()
 
 	app, err := app.NewApp(ctx)
 	if err != nil {
